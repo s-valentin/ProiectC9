7bfcea1085f98ea2142448141ee3329b50c65dda
Vali <56320456+s-valentin@users.noreply.github.com>
2021-12-14 09:15:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import json
 import os
-from objdict import ObjDict
 
 
 def max_depth(startpath):
     max_d = 0
     for path, dirs, files in os.walk(startpath):
-        level = path.replace(startpath, '').count(os.sep)
-        if level > max_d:
-            max_d = level
+        current_d = path.count(os.sep) - startpath.count(os.sep)
+        # print(path, '-->', startpath)
+        if current_d > max_d:
+            max_d = current_d
     return max_d
 
 
@@ -19,6 +19,15 @@ def number_of_files_and_directories(startpath):
         nb_files += len(files)
         nb_dir += len(dirs)
     return nb_files, nb_dir
+
+
+def dir_size(startpath):
+    list_size_dir = {}
+    for path, dirs, files in os.walk(startpath):
+        for d in dirs:
+            dp = os.path.join(path, d)
+            list_size_dir.update({d: get_size(dp)})
+    return list_size_dir
 
 
 def stats_file_extension(startpath, ext):
@@ -76,49 +85,59 @@ def get_size(path):
 
 # MAIN
 
-data = ObjDict()
+data = {}
 folder_input_path = 'C:\\PartitieD\\FACULTATE\\3sem1\\Test'
-json_output_path = 'C:\\PartitieD\\FACULTATE\\3sem1\\Test'
+json_output_path = 'C:\\PartitieD\\FACULTATE\\3sem1'
 
 # path input
-while True:
-    folder_input_path = str(input("Folder input path: "))
-    # list_files(folder_input_path)
-    # print(folder_input_path)
-    if os.path.isdir(folder_input_path):
-        break
-    else:
-        print("Path gresit, reintroduceti path")
+# while True:
+#     folder_input_path = str(input("Folder input path: "))
+#     # list_files(folder_input_path)
+#     # print(folder_input_path)
+#     if os.path.isdir(folder_input_path):
+#         break
+#     else:
+#         print("Path gresit, reintroduceti path")
 
 # path output
-while True:
-    json_output_path = str(input("JSON output path: "))
-    # list_files(json_path_output)
-    # print(json_path_output)
-    if os.path.isdir(json_output_path):
-        break
-    else:
-        print("Path gresit, reintroduceti path")
+# while True:
+#     json_output_path = str(input("JSON output path: "))
+#     # list_files(json_path_output)
+#     # print(json_path_output)
+#     if os.path.isdir(json_output_path):
+#         break
+#     else:
+#         print("Path gresit, reintroduceti path")
 
 # File extensions input
-ext_list = ['.jpg', '.exe']
+ext_list = ['.exe', '.jpg']
 
 print("Input file extensions with dot before and finish with \'0\' ")
 
-while True:
-    x = str(input("\tFile extension:"))
-    if x != "0":
-        ext_list.append(x)
-    else:
-        break
+# while True:
+#     x = str(input("\tFile extension:"))
+#     if x != "0":
+#         ext_list.append(x)
+#     else:
+#         break
+
+x1, x2 = number_of_files_and_directories(folder_input_path)
+x3, x4, x5 = stats_file_extension(folder_input_path, ext_list)
 
 # print(ext_list)
-
-data.max_depth = max_depth(folder_input_path)
-data.number_of_all_files, data.number_of_all_directories = number_of_files_and_directories(folder_input_path)
-data.level_sizes = get_level_size(folder_input_path)
-data.number_of_all_files_by_ext, data.number_of_files_by_ext, data.size_of_files_by_ext = stats_file_extension(
-    folder_input_path, ext_list)
+data.update({'max_depth': max_depth(folder_input_path)})
+data.update({'number_of_all_files': x1})
+data.update({'number_of_all_directories': x2})
+data.update({'level_sizes': get_level_size(folder_input_path)})
+data.update({'number_of_all_files_by_ext': x3})
+data.update({'number_of_files_by_ext': x4})
+data.update({'size_of_files_by_ext': x5})
+data.update({'directory_name_and_size': dir_size(folder_input_path)})
+# data.max_depth = max_depth(folder_input_path)
+# data.number_of_all_files, data.number_of_all_directories = number_of_files_and_directories(folder_input_path)
+# data.level_sizes = get_level_size(folder_input_path)
+# data.number_of_all_files_by_ext, data.number_of_files_by_ext, data.size_of_files_by_ext = stats_file_extension(
+#     folder_input_path, ext_list)
 
 if str(input("Doresti sa exportezi JSON?\n\t")) == "Da":
     json_data = json.dumps(data)
